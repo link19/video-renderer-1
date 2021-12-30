52d701e706cc479b72b2dff24e62c68e4225e988
link19 <38233283+link19@users.noreply.github.com>
2021-12-30 20:59:31 +0800

    Update d3d11_rgb_to_yuv_converter.cc

--- src/video-renderer/d3d11_rgb_to_yuv_converter.cc
+++ src/video-renderer/d3d11_rgb_to_yuv_converter.cc
@@ -2,25 +2,40 @@
 
 using namespace xop;
 
-D3D11RGBToYUVConverter::D3D11RGBToYUVConverter(ID3D11Device* d3d11_device)
-	: d3d11_device_(d3d11_device)
+D3D11RGBToYUVConverter::D3D11RGBToYUVConverter(IDXGISwapChain* swap_chain)
+	: swap_chain_(swap_chain)
 {
-	d3d11_device_->AddRef();
+	swap_chain_->AddRef();
+	swap_chain->GetDevice(__uuidof(ID3D11Device), (void**)&d3d11_device_);
+	d3d11_device_->GetImmediateContext(&d3d11_context_);
 }
 
 D3D11RGBToYUVConverter::~D3D11RGBToYUVConverter()
 {
-	Destroy();
+	Destroy();	
+
+	d3d11_context_->Release();
 	d3d11_device_->Release();
+	swap_chain_->Release();
 }
 
-bool D3D11RGBToYUVConverter::Init(int yuv_width, int yuv_height)
+bool D3D11RGBToYUVConverter::Init(int width, int height)
 {
+	main_texture_.reset(new D3D11RenderTexture(swap_chain_));
+	auxiliary_texture_.reset(new D3D11RenderTexture(swap_chain_));
+
+	D3D11_USAGE usage = D3D11_USAGE_DYNAMIC;
+	UINT bind_flags = D3D11_BIND_SHADER_RESOURCE;
+	UINT cpu_flags = D3D11_CPU_ACCESS_WRITE;
+
+	main_texture_->InitTexture(width, height, DXGI_FORMAT_NV12, usage, bind_flags, cpu_flags, 0);
+	auxiliary_texture_->InitTexture(width, height, DXGI_FORMAT_NV12, usage, bind_flags, cpu_flags, 0);
 
 	return true;
 }
 
 void D3D11RGBToYUVConverter::Destroy()
 {
-
+	main_texture_.reset();
+	auxiliary_texture_.reset();
 }
