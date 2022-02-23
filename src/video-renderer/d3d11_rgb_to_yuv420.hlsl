760166afa01fab525b361198093c4a8766f06973
link19 <38233283+link19@users.noreply.github.com>
2022-02-23 16:35:44 +0800

    Update d3d11_rgb_to_yuv420.hlsl

--- src/video-renderer/d3d11_rgb_to_yuv420.hlsl
+++ src/video-renderer/d3d11_rgb_to_yuv420.hlsl
@@ -9,6 +9,12 @@ struct PixelShaderInput
     float4 color : COLOR0;
 };
 
+cbuffer RGBParams : register(b0)
+{
+    float width;
+    float height;
+};
+
 struct PixelShaderOutput
 {
     float4  YColor   : SV_Target0;
@@ -19,19 +25,22 @@ PixelShaderOutput main(PixelShaderInput input) : SV_TARGET
 {
     PixelShaderOutput Output;
 
-    const float3 offset = float3(0.0625, 0.5, 0.5);
-    const float3 YCoeff = float3(0.213, 0.715, 0.072);
-    const float3 UCoeff = float3(-0.115, -0.385, 0.500);
-    const float3 VCoeff = float3(0.500, -0.454, -0.046);
+    const float3 offset = float3( 0.062,  0.501,  0.501);
+    const float3 YCoeff = float3( 0.257,  0.504,  0.098);
+    const float3 UCoeff = float3(-0.148, -0.291,  0.439);
+    const float3 VCoeff = float3( 0.439, -0.368, -0.071);
+
+    int2   pos = int2(input.uv * float2(width, height));
 
     // YUV420 - B1:Y444(x,y)
     float3 point1 = RGBTexture.Sample(PointSampler, input.uv).rgb;
     float  y = dot(point1, YCoeff) + offset.x;
 
     // YUV420 - B2:U444(2x,2y)  B3:V444(2x,2y)
-    float3 point2 = RGBTexture.Sample(PointSampler, input.uv * 2).rgb;
-    float  u = dot(point2, UCoeff) + offset.y;
-    float  v = dot(point2, VCoeff) + offset.z;
+    float2 image_uv = float2((pos.x * 2) % width / width, (pos.y * 2) % height / height);
+    float3 image_rgb = RGBTexture.Sample(PointSampler, image_uv).rgb;
+    float  u = dot(image_rgb, UCoeff) + offset.y;
+    float  v = dot(image_rgb, VCoeff) + offset.z;
 
     Output.YColor   = float4(y, 0, 0, 0);
     Output.UVClolor = float4(u, v, 0, 0);
